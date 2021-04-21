c4131e4e9357372b9d19522452eaeae50313e0b0
Haluk Bilgiç <ahbilgic1994@gmail.com>
2021-04-21 14:20:34 +0300

    complete abstactClass

---
+++ src/main/java/PageObjectModule/abstracClass.java
@@ -0,0 +1,36 @@
+package PageObjectModule;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import utilities.Driver;
+
+public abstract class  abstracClass {
+
+private WebDriver driver= Driver.getDriver();
+WebDriverWait wait=new WebDriverWait(driver,10);
+
+public void clickFunction(WebElement clickElement){
+    wait.until(ExpectedConditions.elementToBeClickable(clickElement));
+    clickElement.click();
+}
+public void sendKeysFunction(WebElement sendKeysElement,String value){
+    wait.until(ExpectedConditions.visibilityOf(sendKeysElement));
+    sendKeysElement.sendKeys(value);
+}
+public void selectElementFromDropDown(WebElement dropdown,String element){
+    Select slc=new Select(dropdown);
+    slc.selectByVisibleText(element);
+}
+public void assertion(WebElement actual,String expected){
+    wait.until(ExpectedConditions.visibilityOf(actual));
+    Assert.assertEquals(actual.getText(),expected);
+
+    System.out.println("My Message:"+actual.getText());
+}
+
+
+}
